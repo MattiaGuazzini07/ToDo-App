552557b533a0fd5676896b8f0af6126641fa53f1
mattia.guazzini <mattiaguazzini007@gmail.com>
2025-05-06 11:21:44 +0200

    Add login_required decorator to home view

--- DjangoProject1/todo/urls.py
+++ DjangoProject1/todo/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('signup/', views.signup, name='signup'),
 ]
