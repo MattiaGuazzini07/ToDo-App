3c317a8cc6e305450abf94a0f27e3bbad99d33f9
mattia.guazzini <mattiaguazzini007@gmail.com>
2025-05-07 10:40:31 +0200

    Add login_required decorator to home view

--- DjangoProject1/todo/models.py
+++ DjangoProject1/todo/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', 'Bassa'),
+        ('medium', 'Media'),
+        ('high', 'Alta'),
+    ]
+    # Definizione del modello Task
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # last_updated = models.DateTimeField(auto_now=True)
+    due_date = models.DateField(null=True, blank=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+    completed_at = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):
+def __str__(self):
         return self.title
