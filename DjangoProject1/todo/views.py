3e45c679084e6023025f4a67ce919bf66b6fddd1
mattia.guazzini <mattiaguazzini007@gmail.com>
2025-05-07 09:51:31 +0200

    Refactor task management: separate tasks into 'da fare' and 'fatti' lists, enhance UI with Font Awesome icons, and improve logout functionality

--- DjangoProject1/todo/views.py
+++ DjangoProject1/todo/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Task
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from datetime import datetime
 
 @login_required
 def home(request):
@@ -12,19 +13,25 @@ def home(request):
             Task.objects.create(title=title, user=request.user)
             return redirect('home')  # ricarica la pagina
 
-    tasks = Task.objects.filter(user=request.user)
-    return render(request, 'todo/home.html', {'tasks': tasks})
+    tasks_da_fare = Task.objects.filter(user=request.user, completed=False)
+    tasks_fatti = Task.objects.filter(user=request.user, completed=True)
+    return render(request, 'todo/home.html', {
+        'tasks_da_fare': tasks_da_fare,
+        'tasks_fatti': tasks_fatti,
+        'timestamp': datetime.now().timestamp()
+    })
+
 
 @login_required
 def complete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
     task.completed = True
     task.save()
     return redirect('home')
 
 @login_required
 def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
     task.delete()
     return redirect('home')
 
@@ -39,3 +46,10 @@ def signup(request):
         form = UserCreationForm()
 
     return render(request, 'todo/signup.html', {'form': form})
+
+@login_required
+def uncomplete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    task.completed = False
+    task.save()
+    return redirect('home')
