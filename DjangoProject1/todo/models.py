fa1ef923db46430819ca0af0448cfa21199ff2e4
mattia.guazzini <mattiaguazzini007@gmail.com>
2025-05-12 10:52:48 +0200

    add user profile signals and improve CSRF handling in templates

--- DjangoProject1/todo/models.py
+++ DjangoProject1/todo/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
@@ -30,13 +28,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"Profilo di {self.user.username}"
-
-# Crea profilo automaticamente dopo la registrazione
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
--- DjangoProject1/todo/static/todo/js/tour.js
+++ DjangoProject1/todo/static/todo/js/tour.js
@@ -7,6 +7,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("✅ Shepherd caricato");
 
+    // 🔐 Recupera il CSRF token dal cookie
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== "") {
+            const cookies = document.cookie.split(";");
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
     window.startTour = function () {
         const tour = new Shepherd.Tour({
             useModalOverlay: true,
@@ -64,8 +80,10 @@ document.addEventListener("DOMContentLoaded", () => {
                             fetch("/tour/seen/", {
                                 method: "POST",
                                 headers: {
-                                    "X-CSRFToken": window.csrfToken
-                                }
+                                    "X-CSRFToken": getCookie("csrftoken"),
+                                    "Content-Type": "application/json"
+                                },
+                                body: "{}"
                             });
                         }
                     }
