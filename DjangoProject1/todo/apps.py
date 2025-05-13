b5cd48fe6b88709ae4cad0aea547aa8ae2151045
mattia.guazzini <mattiaguazzini007@gmail.com>
2025-05-13 16:18:26 +0200

    🎉 Initialize Django project with environment configuration and dependencies

--- DjangoProject1/todo/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class TodoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todo'
-
-    def ready(self):
-        import todo.signals  # 👈 importa i signals quando l'app è pronta
